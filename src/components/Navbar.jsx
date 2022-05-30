71d3135364bc97595dd177691ec4000db33f294b
ttdesign <tkthedesigner@gmail.com>
2022-05-30 14:26:58 -0700

    add orders page, fix sidebar and navbar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,43 +4,49 @@ import { FiShoppingCart } from 'react-icons/fi';
 import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
-import { Tooltip, TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import avatar from '../data/avatar.jpeg';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
+
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+	<TooltipComponent content={title} position='BottomCenter'>
+		<button
+			type='button'
+			onClick={() => customFunc()}
+			style={{ color }}
+			className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
+			<span
+				style={{ background: dotColor }}
+				className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
+			/>
+			{icon}
+		</button>
+	</TooltipComponent>
+);
+
 const Navbar = () => {
 	const {
+		currentColor,
 		activeMenu,
 		setActiveMenu,
-		isClicked,
-		setIsClicked,
 		handleClick,
-		screenSize,
+		isClicked,
 		setScreenSize,
+		screenSize,
 	} = useStateContext();
 
-	const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
-		<TooltipComponent content={title} position='BottomCenter'>
-			<button
-				type='button'
-				onClick={customFunc}
-				style={{ color }}
-				className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
-				<span
-					style={{ background: dotColor }}
-					className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
-				/>
-				{icon}
-			</button>
-		</TooltipComponent>
-	);
 	useEffect(() => {
 		const handleResize = () => setScreenSize(window.innerWidth);
+
 		window.addEventListener('resize', handleResize);
+
 		handleResize();
+
 		return () => window.removeEventListener('resize', handleResize);
-		//eslint-disable-next-line
 	}, []);
+
 	useEffect(() => {
 		if (screenSize <= 900) {
 			setActiveMenu(false);
@@ -48,42 +54,49 @@ const Navbar = () => {
 			setActiveMenu(true);
 		}
 	}, [screenSize]);
+
+	const handleActiveMenu = () => setActiveMenu(!activeMenu);
+
 	return (
-		<div className='flex justify-between p-2 md:mx6 relative'>
+		<div className='flex justify-between p-2 md:ml-6 md:mr-6 relative'>
 			<NavButton
 				title='Menu'
-				customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
-				color='blue'
+				customFunc={handleActiveMenu}
+				color={currentColor}
 				icon={<AiOutlineMenu />}
 			/>
 			<div className='flex'>
 				<NavButton
 					title='Cart'
 					customFunc={() => handleClick('cart')}
-					color='blue'
+					color={currentColor}
 					icon={<FiShoppingCart />}
 				/>
 				<NavButton
 					title='Chat'
-					customFunc={() => handleClick('chat')}
 					dotColor='#03C9D7'
-					color='blue'
+					customFunc={() => handleClick('chat')}
+					color={currentColor}
 					icon={<BsChatLeft />}
 				/>
 				<NavButton
 					title='Notification'
+					dotColor='rgb(254, 201, 15)'
 					customFunc={() => handleClick('notification')}
-					dotColor='#03C9D7'
-					color='blue'
+					color={currentColor}
 					icon={<RiNotification3Line />}
 				/>
 				<TooltipComponent content='Profile' position='BottomCenter'>
 					<div
 						className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
 						onClick={() => handleClick('userProfile')}>
-						<img src={avatar} alt='avatar' className='rounded-full w-8 h-8' />
+						<img
+							className='rounded-full w-8 h-8'
+							src={avatar}
+							alt='user-profile'
+						/>
 						<p>
-							<span className='text-gray-400 text-14'> Hi,</span>
+							<span className='text-gray-400 text-14'>Hi,</span>{' '}
 							<span className='text-gray-400 font-bold ml-1 text-14'>
 								Michael
 							</span>
@@ -91,6 +104,7 @@ const Navbar = () => {
 						<MdKeyboardArrowDown className='text-gray-400 text-14' />
 					</div>
 				</TooltipComponent>
+
 				{isClicked.cart && <Cart />}
 				{isClicked.chat && <Chat />}
 				{isClicked.notification && <Notification />}
