808ab4c85b479e59cff513a44849575289e1b14b
TK Nguyen <nguyen_kt_2010@yahoo.com>
2024-05-12 20:54:41 -0700

    update ecommerce page, add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
-const Footer = () => {
-  return <div>Footer</div>;
-};
+import React from 'react';
+const Footer = () => (
+	<div className='mt-24'>
+		<p className='dark:text-gray-200 text-gray-700 text-center m-20'>
+			{`© ` + new Date().getFullYear() + ` All rights reserved by Shoppy.com`}
+		</p>
+	</div>
+);
 
 export default Footer;
