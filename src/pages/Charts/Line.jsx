4399d8d81b8aeb382b6904a25f699b60359e0499
TK Nguyen <nguyen_kt_2010@yahoo.com>
2024-05-12 19:55:15 -0700

    add line chart

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Header, LineChart } from '../../components';
 
 const Line = () => {
-  return (<div>Line</div>)
+	return (
+		<div className='m-4 md:m-10 m5-24 pt-10 bg-white dark:bg-secondary-dark-bg rounded 3xl'>
+			<Header category='Chart' tittle='Inflation Rate' />
+			<div className='w-full'>
+				<LineChart />
+			</div>
+		</div>
+	);
 };
 
 export default Line;
